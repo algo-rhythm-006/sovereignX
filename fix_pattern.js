8dc02c994956816a24048f99f2f701e47df5ebe3
prithvi-raj-thakur <05rajprithvi@gmail.com>
2026-09-23 12:48:47 +0530

    chore: remove temporary HTML-to-JSX conversion and extraction utility scripts

--- fix_pattern.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('c:/Users/Prithvi/Desktop/hack-spire/auth/Login.jsx', 'utf8');
-code = code.replace(/_next\/image\?url=%2F_next%2Fstatic%2Fmedia%2Fform-pattern\.270a3962\.jpg/g, 'images/form-pattern.jpg');
-fs.writeFileSync('c:/Users/Prithvi/Desktop/hack-spire/auth/Login.jsx', code);
--- fix_srcset.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('c:/Users/Prithvi/Desktop/hack-spire/auth/Login.jsx', 'utf8');
-code = code.replace(/srcSet=['"].*?['"]/g, '');
-fs.writeFileSync('c:/Users/Prithvi/Desktop/hack-spire/auth/Login.jsx', code);
